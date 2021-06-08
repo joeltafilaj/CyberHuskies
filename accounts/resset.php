658128498db04f1bc4cca6627d396b5291235710
Joel Tafilaj <tafilaj82@gmail.com>
2021-06-08 14:03:19 +0200

    Bug Fix

--- accounts/resset.php
+++ accounts/resset.php
@@ -8,43 +8,48 @@ if (isset($_GET['token'])) {
 
     // Verify token on database
     $vkey = test_input($_GET['token']);
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/CyberHuskies/inc/db_connection.php';
-    $sqlSearchToken = "SELECT vkey FROM User WHERE vkey = '$vkey' AND verified = 1 LIMIT 1";
-    $resultSearchToken = mysqli_query($connection, $sqlSearchToken);
-    if (mysqli_num_rows($resultSearchToken) != 0) {
-        $validToken = true;
+    $result = is_numeric($vkey);
+    if ($result == true) {
+        require_once $_SERVER['DOCUMENT_ROOT'] . '/CyberHuskies/inc/db_connection.php';
+        $sqlSearchToken = "SELECT vkey FROM User WHERE vkey = '$vkey' AND verified = 1 LIMIT 1";
+        $resultSearchToken = mysqli_query($connection, $sqlSearchToken);
+        if (mysqli_num_rows($resultSearchToken) != 0) {
+            $validToken = true;
 
-        // Resset Password Form
-        if (isset($_POST['newPassword']) && isset($_POST['newPasswordConfirmation'])) {
-            
-            // Server Side validation of fields
-            if (empty($_POST['newPassword'])) {
-                $message = "Enter a password";
-            } else {
-                $newPassword = test_input($_POST['newPassword']);
-                $passwordConfirmation = test_input($_POST['newPasswordConfirmation']);
-                if (strlen($newPassword) < 8) {
-                    $message = "Create a password at least 8 characters long.";
-                    $validatedForm = false;
-                } elseif (!empty($newPassword) && $passwordConfirmation != $newPassword) {
-                    $message = "Those passwords didn’t match. Try again.";
+            // Resset Password Form
+            if (isset($_POST['newPassword']) && isset($_POST['newPasswordConfirmation'])) {
+
+                // Server Side validation of fields
+                if (empty($_POST['newPassword'])) {
+                    $message = "Enter a password";
                 } else {
-                    $validatedForm = true;
-                    $newPassword = hash('sha512', $newPassword);
+                    $newPassword = test_input($_POST['newPassword']);
+                    $passwordConfirmation = test_input($_POST['newPasswordConfirmation']);
+                    if (strlen($newPassword) < 8) {
+                        $message = "Create a password at least 8 characters long.";
+                        $validatedForm = false;
+                    } elseif (!empty($newPassword) && $passwordConfirmation != $newPassword) {
+                        $message = "Those passwords didn’t match. Try again.";
+                    } else {
+                        $validatedForm = true;
+                        $newPassword = hash('sha512', $newPassword);
+                    }
                 }
-            }
-            if ($validatedForm) {
-                $sqlUpdatePassword = "UPDATE User SET password = '$newPassword' WHERE vkey = '$vkey'";
-                if (mysqli_query($connection, $sqlUpdatePassword)) {
-                    $message = 'Password changed successfully';
-                } else {
-                    $validatedForm = false;
-                    $message = 'Server Error. Try again later.';
+                if ($validatedForm) {
+                    $sqlUpdatePassword = "UPDATE User SET password = '$newPassword' WHERE vkey = '$vkey'";
+                    if (mysqli_query($connection, $sqlUpdatePassword)) {
+                        $message = 'Password changed successfully';
+                    } else {
+                        $validatedForm = false;
+                        $message = 'Server Error. Try again later.';
+                    }
                 }
             }
         }
+        mysqli_close($connection);
+    } else {
+        $validToken = false;
     }
-    mysqli_close($connection);
 }
 
 ?>
@@ -57,19 +62,45 @@ if (isset($_GET['token'])) {
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
     <!--Jquery -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
     <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
     </script>
 
     <!--Font Awesome -->
     <link rel="stylesheet" type="text/css" href="../inc/fontawesome-5-pro-master/css/all.css">
+
+    <style>
+        body {
+            font-family: "Mulish", sans-serif;
+        }
+
+        .otherProduct-header {
+            color: rgb(146, 114, 43);
+            font-weight: 500;
+            font-size: 40px;
+            margin-bottom: 3rem;
+            text-align: center;
+        }
+
+        .otherProduct-header:after {
+            content: "";
+            width: 5%;
+            height: 2px;
+            background-color: #663300;
+            display: block;
+            margin: 14px auto 8px;
+            transition: 0.2s;
+        }
+
+        .otherProduct-header:hover:after {
+            width: 25%;
+        }
+    </style>
 
     <!--Javascript -->
     <script src="../inc/js/resset.js"></script>
@@ -79,73 +110,70 @@ if (isset($_GET['token'])) {
 <?php
 
 // Checking if token is valid then show the resset password form
+
 if ($validToken) {
 ?>
 
-<body style="background-color: whitesmoke;">
-    <br><br><br><br><br><br>
-    <div class="container-fluid">
-        <form id="ressetPasswordForm" action="resset.php<?php echo "?token=" . $vkey; ?>" method="post"
-            class="row justify-content-center"><br>
-            <div class="col-sm-3 bg-light" style="border: 2px solid lightgrey;">
+    <body style="background-color: whitesmoke;">
+        <br><br><br><br><br><br>
+        <div class="container-fluid">
+            <form id="ressetPasswordForm" action="resset.php<?php echo "?token=" . $vkey; ?>" method="post" class="row justify-content-center"><br>
+                <div class="col-sm-3 bg-light" style="border: 2px solid lightgrey;">
 
-                <!-- New Password -->
-                <div class="form-group row justify-content-center px-5 mt-5">
-                    <div class="col-sm-12 text-center mb-2">
-                        <h2>Password Resset</h2>
-                    </div>
+                    <!-- New Password -->
+                    <div class="form-group row justify-content-center px-5 mt-5">
+                        <div class="col-sm-12 text-center mb-2">
+                            <h2>Password Resset</h2>
+                        </div>
 
-                    <!-- Response for password -->
-                    <span id="ressetPasswordMessage" class="col-sm-11 mt-4 text-center mb-5 alert 
+                        <!-- Response for password -->
+                        <span id="ressetPasswordMessage" class="col-sm-11 mt-4 text-center mb-5 alert 
                     <?php if ($validatedForm === true) {
                         echo 'alert-success';
                     } else {
                         echo 'alert-danger';
                     } ?> mt-1"><?php echo $message ?></span>
 
-                    <div class="col-sm-12 form-floating">
-                        <input type="password" id="newPassword" name="newPassword" placeholder="New password"
-                            class="form-control">
-                        <label class="ms-2" for="newPassword">New password</label>
-                    </div>
-                </div><br>
+                        <div class="col-sm-12 form-floating">
+                            <input type="password" id="newPassword" name="newPassword" placeholder="New password" class="form-control">
+                            <label class="ms-2" for="newPassword">New password</label>
+                        </div>
+                    </div><br>
 
-                <!-- New Password Confirmation -->
-                <div class="form-group row justify-content-center px-5">
-                    <div class="col-sm-12 form-floating">
-                        <input type="password" id="newPasswordConfirmation" name="newPasswordConfirmation"
-                            placeholder="New password confirmation" class="form-control" aria-label="Password..."
-                            aria-describedby="but">
-                        <label class="ms-2" for="newPasswordConfirmation">New password confirmation</label>
-                    </div>
-                </div><br>
+                    <!-- New Password Confirmation -->
+                    <div class="form-group row justify-content-center px-5">
+                        <div class="col-sm-12 form-floating">
+                            <input type="password" id="newPasswordConfirmation" name="newPasswordConfirmation" placeholder="New password confirmation" class="form-control" aria-label="Password..." aria-describedby="but">
+                            <label class="ms-2" for="newPasswordConfirmation">New password confirmation</label>
+                        </div>
+                    </div><br>
 
-                <!-- Resset Password Button -->
-                <div class="form-group row justify-content-center px-5 mb-5">
-                    <div class="col-sm-12">
-                        <button type="submit" id="submitRessetPasswordForm" name="submitRessetPasswordForm"
-                            class="btn btn-primary w-100" form="ressetPasswordForm" style="height: 50px;"> Resset
-                            Password
-                        </button>
-                    </div>
-                </div><br>
-            </div>
-        </form>
-    </div>
-</body>
+                    <!-- Resset Password Button -->
+                    <div class="form-group row justify-content-center px-5 mb-5">
+                        <div class="col-sm-12">
+                            <button type="submit" id="submitRessetPasswordForm" name="submitRessetPasswordForm" class="btn btn-primary w-100" form="ressetPasswordForm" style="height: 50px;"> Resset
+                                Password
+                            </button>
+                        </div>
+                    </div><br>
+                </div>
+            </form>
+        </div>
+    </body>
 <?php
+
 } else {
 ?>
 
-<body style="background-color: lightgrey;">
-    <br><br><br><br><br>
-    <div class="container-fluid text-center">
-        <h1 class="mt-5"
-            style="font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;">
-            Password Resset failed ! <i class="fad fa-frown"></i></h1> <br>
-        <span class="h7">Either the link had already expired or you did not copy the URL properly.</span>
-    </div>
-</body>
+    <body>
+        <br><br><br><br><br>
+        <div class="container-fluid text-center">
+            <h1 class="mt-5 otherProduct-header">
+                    Password Resset failed ! <i class="fad fa-frown"></i> <br>
+                <span class="h5">Either the link had already expired or you did not copy the URL properly.</span>
+            </h1>
+        </div>
+    </body>
 <?php
 }
 ?>
--- accounts/verified.php
+++ accounts/verified.php
@@ -7,25 +7,31 @@ $valid = 'danger';
 if ($_SERVER['REQUEST_METHOD'] == 'GET') {
   if (isset($_GET['token'])) {
     $vkey = test_input($_GET['token']);
+    $result = is_numeric($vkey);
+    if ($result) {
 
-    //DB connection to check if the verification key corresponds to a certain account
-    require_once $_SERVER['DOCUMENT_ROOT'] . '/CyberHuskies/inc/db_connection.php';
-    $sqlEmailVerification = "SELECT vkey FROM User WHERE vkey = '$vkey' AND verified = 0";
-    $resultEmailVerification = mysqli_query($connection, $sqlEmailVerification);
-    if (mysqli_num_rows($resultEmailVerification) == 1) {
-      $sqlVerified = "UPDATE User SET verified = 1 WHERE vkey = '$vkey'";
-      if (mysqli_query($connection, $sqlVerified)) {
-        $response = 'Your account has been verified successfully ! <i class="fad fa-smile-beam"></i>';
-        $valid = 'success';
+      //DB connection to check if the verification key corresponds to a certain account
+      require_once $_SERVER['DOCUMENT_ROOT'] . '/CyberHuskies/inc/db_connection.php';
+      $sqlEmailVerification = "SELECT vkey FROM User WHERE vkey = '$vkey' AND verified = 0";
+      $resultEmailVerification = mysqli_query($connection, $sqlEmailVerification);
+      if (mysqli_num_rows($resultEmailVerification) == 1) {
+        $sqlVerified = "UPDATE User SET verified = 1 WHERE vkey = '$vkey'";
+        if (mysqli_query($connection, $sqlVerified)) {
+          $response = 'Your account has been verified successfully ! <i class="fad fa-smile-beam"></i>';
+          $valid = 'success';
+        } else {
+          $response = 'Email verification failed ! <i class="fad fa-frown"></i>';
+          $response2 = 'Either the link had already expired or you did not copy the URL properly.';
+        }
       } else {
         $response = 'Email verification failed ! <i class="fad fa-frown"></i>';
         $response2 = 'Either the link had already expired or you did not copy the URL properly.';
       }
+      mysqli_close($connection);
     } else {
       $response = 'Email verification failed ! <i class="fad fa-frown"></i>';
       $response2 = 'Either the link had already expired or you did not copy the URL properly.';
     }
-    mysqli_close($connection);
   } else {
     $response = 'Email verification failed ! <i class="fad fa-frown"></i>';
     $response2 = 'Either the link had already expired or you did not copy the URL properly.';
@@ -39,35 +45,35 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Auction</title>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+  <title>Auction</title>
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous" />
 
-    <!--Jquery -->
-    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <!--Jquery -->
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
-    <!-- Bootstrap JavaScript -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js"
-        integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
-    </script>
+  <!-- Bootstrap JavaScript -->
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous">
+  </script>
 
-    <!--Font Awesome -->
-    <link rel="stylesheet" type="text/css" href="../inc/fontawesome-5-pro-master/css/all.css">
-    <style>
+  <!--Font Awesome -->
+  <link rel="stylesheet" type="text/css" href="../inc/fontawesome-5-pro-master/css/all.css">
+  <style>
     body {
       font-family: "Mulish", sans-serif;
     }
+
     .otherProduct-header {
-    color: rgb(146, 114, 43);
-    font-weight: 500;
-    font-size: 40px;
-    margin-bottom: 3rem;
-    text-align: center;
+      color: rgb(146, 114, 43);
+      font-weight: 500;
+      font-size: 40px;
+      margin-bottom: 3rem;
+      text-align: center;
     }
+
     .otherProduct-header:after {
       content: "";
       width: 5%;
@@ -77,22 +83,24 @@ if ($_SERVER['REQUEST_METHOD'] == 'GET') {
       margin: 14px auto 8px;
       transition: 0.2s;
     }
+
     .otherProduct-header:hover:after {
       width: 25%;
     }
-    </style>
+  </style>
 </head>
 
 <body><br><br><br><br><br>
-    <div class="container-fluid">
-        <div class="row text-center mt-5 text-dark">
-            <div class="col-lg-12">
-                <h1 class="otherProduct-header"> <?php echo $response; ?>
-                <br>
-                <span class="h5"><?php echo $response2; ?></span></h1>
-            </div>
-        </div>
+  <div class="container-fluid">
+    <div class="row text-center mt-5 text-dark">
+      <div class="col-lg-12">
+        <h1 class="otherProduct-header"> <?php echo $response; ?>
+          <br>
+          <span class="h5"><?php echo $response2; ?></span>
+        </h1>
+      </div>
     </div>
+  </div>
 </body>
 
 </html>
