a2d40fc442aa132759c16c789a84cf2de3832552
Joel Tafilaj <tafilaj82@gmail.com>
2021-06-08 14:28:06 +0200

    Bug Fix

--- inc/php/logout.php
+++ inc/php/logout.php
@@ -2,6 +2,7 @@
 
 //Deleting sessions after log-out and referencing to log in page
 Session_start();
+if (isset($_SESSION['username'])) {
 
 //DB Conncetion to make user 'is_active' false and delete 'Cookie' after log out
 require_once $_SERVER['DOCUMENT_ROOT'] . '/CyberHuskies/inc/db_connection.php';
@@ -15,5 +16,8 @@ if (mysqli_query($connection, $sql)) {
     $page = $_SERVER['HTTP_REFERER'];
     header('Location:'.$page.'');
 } else {
-    header('Location:../error.html');
+    header('Location:../html/error.html');
+}
+} else {
+    header('Location:../html/error.html');
 }
